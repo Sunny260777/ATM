8f92b4a2c5e4a1b375aba967fbc7d10a2efd922a
sunny Bhardwaj <sbsunnyBhardwajsb2015@outlook.com>
2023-04-01 21:08:03 +0530

    initiate

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -6,7 +6,25 @@ namespace ATM
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int amount = 2034, deposit, withdraw;
+            int choice, pin = 0;
+            Console.WriteLine("************ Welcome To Bank ****************");
+            Console.WriteLine("Please enter you pin");
+            pin = Convert.ToInt32(Console.ReadLine());
+
+            if (pin==2607){
+                Console.Clear();
+                Console.WriteLine("Welcome Sunny Bhardwaj \n");
+                Console.WriteLine("1. Current Balance\n");
+                Console.WriteLine("2. Withdraw \n");
+                Console.WriteLine("3. Deposit \n");
+                Console.WriteLine("4. Cancel \n");
+            }
+            else{
+                Console.WriteLine("Wrong Ping Try again later");
+            }
+
+            Console.ReadLine();
         }
     }
 }
